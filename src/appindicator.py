a6f8cd137d1e0a8a200ff141e4ed210ae4f36195
armsp <shantamdps@gmail.com>
2018-10-27 09:05:13 +0530

    Fixed warning messages when script is run
    Removed warning messages by specifying the correct version of Gtk and AppIndicator required by the script.

--- src/appindicator.py
+++ src/appindicator.py
@@ -1,13 +1,20 @@
 import os
 from collections import deque
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk, GLib
 from gi.repository import AppIndicator3 as appindicator
 
+# Globals
 APPINDICATOR_ID = 'myappindicator'
 DEQUE_LEN = 5
 circular_buf = deque(maxlen=DEQUE_LEN)
 
 def change_label(ind_app):
+    '''Update the label text of the appindicator.
+    Also updates the icon color based on the mean power.'''
     power = find_power()
     if power in ('Charging', 'Unknown', 'Full', 'Undefined'):
         ind_app.set_label(power, '8.8')
@@ -26,10 +33,12 @@ def change_label(ind_app):
             ind_app.set_icon(os.path.abspath('icons/microchip_red.svg'))
         return True
 
+# Quit menu
 def quit(source):
     Gtk.main_quit()
 
 def get_readings():
+    '''Reads the status, current_now and voltage_now files for their values and returns a tuple containing all three'''
     try:
         with open('/sys/class/power_supply/BAT0/status', 'r') as status_file:
             charging_status = status_file.read()
@@ -53,6 +62,7 @@ def get_readings():
         return None, None, None
 
 def find_power():
+    '''Gets the readings from "get_readings" function and calculates the power consumption in SI units'''
     status, I, V = get_readings()
     if status == None or I == None or V == None:
         return 'Undefined'
@@ -62,15 +72,16 @@ def find_power():
         power = (int(I)/1000000)*(int(V)/1000000)
         return power
 
+#Define the indicator app
 ind_app = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('icons/microchip.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
 ind_app.set_status(appindicator.IndicatorStatus.ACTIVE)
 
-# create a menu
+# Create a menu
 menu = Gtk.Menu()
-menu_items = Gtk.MenuItem("Exit")
+menu_items = Gtk.MenuItem("Exit") #Add Exit to the menu
 menu.append(menu_items)
 menu_items.connect("activate", quit)
 menu_items.show_all()
 ind_app.set_menu(menu)
-GLib.timeout_add(3000, change_label, ind_app)
+GLib.timeout_add(3000, change_label, ind_app) #Call change_label function every 3 seconds
 Gtk.main()
